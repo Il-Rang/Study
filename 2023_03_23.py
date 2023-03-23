045739828b0190e9d1e7dd0bbb4dde41dc2bf40b
Il-Rang <kim.sw.040408@gmail.com>
2023-03-23 11:07:56 +0900

    03.23

--- 2023_03_23.py
+++ 2023_03_23.py
@@ -1,28 +1,47 @@
-# sun = 0
+# sum = 0
 # n1, n2 = 0, 0
 
 # while True :
 #     n1 = int(input("num1 → "))
 #     n2 = int(input("num1 → "))
 
-#     sun = n1 + n2
+#     sum = n1 + n2
 #     print(f'{n1} + {n2} = {sum}')
 
 #=======================================================
 
-sun = 0
-n1, n2 = 0, 0
+# for i in range(5):
+#     for j in range(5):
+#         if j == 2:
+#             continue
+#         print(f'i = {i}, j = {j}')
 
-while True :
-    n1 = int(input("num1 → "))
-    if n1 == 0:
-        break
-    n2 = int(input("num1 → "))
+#=======================================================
 
-    sun = n1 + n2
-    print(f'{n1} + {n2} = {sum}')
+# sum = 0
+# n1, n2 = 0, 0
 
-print("0입 입력해서 계산을 종료합니다.")
+# while True :
+#     n1 = int(input("num1 → "))
+#     if n1 == 0:
+#         break
+#     n2 = int(input("num1 → "))
+
+#     sum = n1 + n2
+#     print(f'{n1} + {n2} = {sum}')
+
+# print("0을 입력해서 계산을 종료합니다.")
+
+#=======================================================
+
+# sum = 0
+# for i in range(1, 101, 1):
+#     if i % 4 == 0:
+#         continue
+
+#     sum += i
+
+# print(f'1~100까지의 합계(4의 배수 제외) = {sum}')
 
 #=======================================================
 
