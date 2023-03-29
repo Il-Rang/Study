2261e6e0d44142a2c189050c2b49e064046f747e
Il-Rang <kim.sw.040408@gmail.com>
2023-03-29 15:56:53 +0900

    03.28 CORRECT

--- 2023_03_28.py
+++ 2023_03_28.py
@@ -54,36 +54,36 @@
 
 # # =============================================================================
 
-# nList = list(range(10))
-# print(f"1 : {nList}")
+nList = list(range(10)) # 0 ~ 9까지의 값을 지닌 리스트를 생성
+print(f"1 : {nList}")
 
-# nList[1] = 100
-# print(f"2 : {nList}")
+nList[1] = 100 # 1번 인덱스의 값을 100으로 변경
+print(f"2 : {nList}")
 
-# sum =  0
-# for i in nList:
-#     sum += i
-# print(f"3 : {nList}")
+sum =  0 # 리스트 내 값의 합을 출력
+for i in nList:
+    sum += i
+print(f"3 : {nList}")
 
-# nList[2] = "not number"
-# print(f"4 : {nList}")
+nList[2] = "not number" # 2번 인덱스의 값을 "not number"로 변경
+print(f"4 : {nList}")
 
-# for j in range(10, 21):
-#     nList.append(j)
-# print(f"5 : {nList}")
+for j in range(10, 21): # 리스트에 10 ~ 20까지의 값을 추가
+    nList.append(j)
+print(f"5 : {nList}")
 
-# print(f"6 : {len(nList)}")
+print(f"6 : {len(nList)}") # 리스트의 크기를 출력
 
-# print(f"7 : {nList[10:-1]}")
-# print(f"8 : {nList[0:2]}")
+print(f"7 : {nList[10:-1]}") # 10번 인덱스부터 마지막까지의 값을 출력
+print(f"8 : {nList[0:2]}") # 0번 인덱스 부터 2번 인덱스까지의 값을 출력
 
-# for x in range(len(nList)):
-#     if type(nList[x]) is str:
-#         print(f"9 : 리스트의 {x}번 인덱스 값이 문자열입니다.")
-#         nList[x] = x
-# print(f"9 : {nList}")
+for x in range(len(nList)): # 값이 문자열인 인덱스를 찾아 값을 인덱스로 변경
+    if type(nList[x]) is str:
+        print(f"9 : {x}번 인덱스의 값이 문자열입니다.")
+        nList[x] = x
+print(f"9 : {nList}")
 
-# nList = [i for i in nList if i < 10]
-# print(f"10 : {nList}")
+nList = [i for i in nList if i < 10] # 리스트 내 10이상의 값을 삭제
+print(f"10 : {nList}")
 
 # # =============================================================================
